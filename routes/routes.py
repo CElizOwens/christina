06947fc23279631d6b8494a468a56b1f26087cc4
Christina Owens <owens.christina@gmail.com>
2020-05-22 17:48:32 -0700

    Create v2 html and css files, add routes for them

--- routes/routes.py
+++ routes/routes.py
@@ -9,6 +9,15 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/index_v2')
+def index_v2():
+    return render_template('index_v2.html')
+
+@app.route('/about')
+def about():
+    return render_template('about_v2.html')
+
+
 @app.route('/scratch')
 def scratch():
     return render_template('scratch.html')
