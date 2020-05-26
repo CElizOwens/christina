f54df846bec3d02bc65c9606069369a04e7d1ec4
Christina Owens <owens.christina@gmail.com>
2020-05-25 17:15:04 -0700

    Move templates and static up a level into 'website' directory Rename old 'routes/' and 'routes.py' to 'views' and 'views.py'

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import app
-from routes import routes   # Will say import is unused; is used at runtime
+from views import views   # Will say import is unused; is used at runtime
 
 if __name__ == "__main__":
     app.run(debug=True)
