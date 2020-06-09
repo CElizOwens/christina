ef449c9aea95a88f70dd923d9d1fe19c717806a7
Christina Owens <owens.christina@gmail.com>
2020-06-08 20:02:58 -0700

    Create inner join between piece and composer tables Populate repertoire page using this new inner join

--- website/persistence/persistence.py
+++ website/persistence/persistence.py
@@ -8,7 +8,7 @@ engine = create_engine(databaseURI, echo=True)
 
 def get_all_pieces():
     with engine.connect() as con:
-        result = con.execute("SELECT p.composer, p.title, p.opus, p.date_played, e.ens_type FROM piece p INNER JOIN ensemble e ON p.ensemble_id = e.id;")
+        result = con.execute("SELECT c.last_name AS composer, p.title, p.opus, p.date_played, e.ens_type FROM piece p INNER JOIN ensemble e ON p.ensemble_id = e.id INNER JOIN composer c ON p.composer_id = c.id;")
         pieces = []
         for row in result:
             pieces.append(Piece(**row))
