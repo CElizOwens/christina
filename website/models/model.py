8d301f6a5873e3d13813ae7f62794910572458d7
Christina Owens <owens.christina@gmail.com>
2020-10-08 15:45:36 -0700

    Change code so that 'first_inits' from composer table is no longer used and Repertoire page shows only composer last names. In website folder... -> models/model.py: remove 'first_inits' from Piece namedtuple -> persistence/persistence.py: remove 'c.first_inits' from result variable -> templates/admin/repertoire.html: In loop '{% for piece in piece %}',    comment out '{{ piece.first_inits }}'

--- website/models/model.py
+++ website/models/model.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 
-Piece = namedtuple('Piece', ['composer', 'title', 'opus', 'ens_type'])
+Piece = namedtuple('Piece', ['last_name', 'title', 'opus', 'ens_type'])
+# ['last_name', 'first_inits', 'title', 'opus', ens_type']
 Ensemble = namedtuple('Ensemble', ['id', 'ens_type', 'size'])
 Event = namedtuple('Event', ['id', 'location', 'day_time'])
