56d3613c6864cbd5eea38b4d4b43b9261001c555
Christina Owens <owens.christina@gmail.com>
2020-10-21 17:28:03 -0700

    config.py:  - Change 'testDatabaseURI' to 'test_databaseURI' persistence.py:  - Import 'test_databaseURI'  - In get_all_pieces(), no substantial change,    merely commented out original contest of function    and wrote a new SQL call and for-loop to play with,    only difference is 'LIMIT 10' since using all data    from piece and composer tables

--- website/persistence/persistence.py
+++ website/persistence/persistence.py
@@ -1,25 +1,35 @@
-from website.models.model import Piece, Event # , Ensemble
-from website.config import databaseURI
+from website.models.model import Piece, Event  # , Ensemble
+from website.config import databaseURI, test_databaseURI
 from sqlalchemy import create_engine, text
 
 
-engine = create_engine(databaseURI, echo=True)
+# engine = create_engine(databaseURI, echo=True)
+engine = create_engine(test_databaseURI, echo=True)
 
 
 def get_all_pieces():
     with engine.connect() as con:
         # result = con.execute("SELECT c.name, p.title, p.opus, e.ens_type FROM piece p INNER JOIN ensemble e ON p.ensemble_id = e.id INNER JOIN composer c ON p.composer_id = c.id;")
-        result = con.execute("SELECT c.name, p.title FROM piece p INNER JOIN composer c ON p.composer_id = c.id;")
-        pieces = []
-        for row in result:
-            pieces.append(Piece(**row))
-    return pieces
+
+    #     result = con.execute("SELECT c.name, p.title FROM piece p INNER JOIN composer c ON p.composer_id = c.id;")
+    #     pieces = []
+    #     for row in result:
+    #         pieces.append(Piece(**row))
+    # return pieces
+
+        res = con.execute("SELECT c.name, p.title FROM piece p INNER JOIN composer c ON p.composer_id = c.id LIMIT 10;")
+
+        repertoire = []
+        for row in res:
+            repertoire.append(Piece(**row))
+    return repertoire
 
 
-def insert_piece(name, title): # , opus, ensemble_id):
+def insert_piece(name, title):  # , opus, ensemble_id):
     with engine.connect() as con:
         # con.execute(text("INSERT INTO piece (composer, title, opus, ensemble_id) VALUES (:composer, :title, :opus, :ensemble_id);"), composer=composer, title=title, opus=opus, ensemble_id=ensemble_id)
         con.execute(text("INSERT INTO piece (name, title) VALUES (:name, :title);"), name=name, title=title)
+
 
 def insert_event(location, day_time):
     with engine.connect() as con:
